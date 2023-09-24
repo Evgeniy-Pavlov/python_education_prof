b293e305b0e17b1410835d803e800de64623d8b3
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2023-09-24 19:13:41 +0300

    исправлены отступы

--- homework_03/api.py
+++ homework_03/api.py
@@ -71,7 +71,6 @@ class Field(metaclass=abc.ABCMeta):
         на заявленные требования к данным указанным в поле."""
         self.validate_field(value)
         value = self.check_value_type(value)
-        
         if not value:
             return value
         self.check_requirements(value)
@@ -79,7 +78,6 @@ class Field(metaclass=abc.ABCMeta):
 
 
 class CharField(Field):
-    
     def check_value_type(self, value: str):
         if value is not None and not isinstance(value, str):
             raise TypeError('Переданное значение не является строкой')
@@ -95,34 +93,36 @@ class ArgumentsField(Field):
 
 
 class EmailField(CharField):
+
     """email - строĸа, в ĸоторой есть @, опционально, может быть пустым"""
-    
+
     def check_requirements(self, value: str):
         super().check_requirements(value)
-        pattern = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', re.IGNORECASE)
+        pattern = re.compile(
+            r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', re.IGNORECASE)
         match_email = re.fullmatch(pattern, value)
         if not re.fullmatch(pattern, value):
             raise ValueError('Неверный формат адреса почты')
 
 
 class PhoneField(Field):
+
     """phone - строĸа или число, длиной 11, начинается с 7, опционально, может быть пустым"""
 
     def check_value_type(self, value):
         if value is None:
             return value
         if not isinstance(value, (str, int)):
-            raise TypeError('Переданное значение не является строкой или целым числом')
+            raise TypeError(
+                'Переданное значение не является строкой или целым числом')
         return str(value)
-    
+
     def check_requirements(self, value: str):
         if not str(value).isdigit():
             raise ValueError("Это поле должно содержать только цифры")
         str_value = str(value)
         if str_value[0] != '7' or len(str_value) != 11:
             raise ValueError('Неверный формат номера телефона')
-
-
 
 
 class DateField(CharField):
@@ -133,33 +133,36 @@ class DateField(CharField):
             return value
         try:
             return datetime.datetime.strptime(value, "%d.%m.%Y").date()
-        except ValueError:
+        except Exception:
             raise ValueError('Неверный формат даты. Формат DD.MM.YYYY')
-    
 
 
 class BirthDayField(DateField):
-    
+
     def check_requirements(self, value):
         super().check_requirements(value)
         today = datetime.date.today()
-        if (today - value).days / 365.25  > MAX_AGE:
-            raise ValueError(f'Превышено максимальное значение возраста, максимально допустимое значение = {MAX_AGE} лет')
+        if (today - value).days / 365.25 > MAX_AGE:
+            raise ValueError(f'Превышено максимальное значение возраста, \
+            максимально допустимое значение = {MAX_AGE} лет')
 
 
 class GenderField(Field):
 
     def check_value_type(self, value: int):
         if value is not None and not isinstance(value, int):
-            raise TypeError('Переданное значение не является целым числом. Допустимые значения: 0, 1, 2')
+            raise TypeError(
+                'Переданное значение не является целым числом. Допустимые значения: 0, 1, 2')
         return value
-    
+
     def check_requirements(self, value: int):
-        if value not in GENDERS.keys():
-            raise ValueError('Переданное значение пола не является допустимым.')
+        if value not in GENDERS:
+            raise ValueError(
+                'Переданное значение пола не является допустимым.')
 
 
 class ClientIDsField(Field):
+
     """client_ids - массив чисел, обязательно, не пустое"""
 
     def check_value_type(self, value: list):
@@ -167,12 +170,13 @@ class ClientIDsField(Field):
             if not isinstance(value, list):
                 raise TypeError('Переданное значение не является списком')
             elif [x for x in value if not isinstance(x, int)]:
-                raise ValueError('В переданном списке присутствует значения не являющиеся целыми числами.')
+                raise ValueError(
+                    'В переданном списке присутствует значения не являющиеся целыми числами.')
         return value
-    
+
     def check_requirements(self, value: list):
-         if [x for x in value if x < 0]:
-             raise ValueError('В списке присутствуют недопустимые значения.')
+        if [x for x in value if x < 0]:
+            raise ValueError('В списке присутствуют недопустимые значения.')
 
 
 class RequestMeta(type):
@@ -199,16 +203,16 @@ class Request(metaclass=RequestMeta):
         self.fields_errors = None
         self.data = {} if not data else data
         self.non_empty_fields = []
-    
+
     @property
     def errors(self):
         if self.fields_errors is None:
             self.validate()
         return self.fields_errors
-    
+
     def is_valid(self):
         return not self.errors
-    
+
     def validate(self):
         self.fields_errors = {}
         for name, field in self.fields_dict.items():
@@ -228,9 +232,11 @@ class ClientsInterestsRequest(Request):
 
 
 class OnlineScoreRequest(Request):
+
     """Валидация аргументов аргументы валидны, если валидны все поля по
     отдельности и если присутсвует хоть одна пара phone-email, first name-last name,
     gender-birthday с непустыми значениями. Проверяется в функции validate."""
+    
     first_name = CharField(required=False, nullable=True)
     last_name = CharField(required=False, nullable=True)
     email = EmailField(required=False, nullable=True)
@@ -241,9 +247,9 @@ class OnlineScoreRequest(Request):
     def validate(self):
         super().validate()
         if not self.fields_errors:
-            if not (self.phone and self.email) and not (self.first_name and self.last_name) and not (self.gender is not None and self.birthday):
+            if not (self.phone and self.email) and not (self.first_name and self.last_name) \
+                    and not (self.gender is not None and self.birthday):
                 self.fields_errors['arguments'] = 'Присутствуют невалидные аргументы.'
-            
 
 
 class MethodRequest(Request):
@@ -267,11 +273,11 @@ class OnlineScoreHandler:
         r = OnlineScoreRequest(request.arguments)
         if not r.is_valid():
             return r.errors, INVALID_REQUEST
-        
         if request.is_admin:
             score = 42
         else:
-            score = scoring.get_score(store, r.phone, r.email, r.birthday, r.gender, r.first_name, r.last_name)
+            score = scoring.get_score(
+                store, r.phone, r.email, r.birthday, r.gender, r.first_name, r.last_name)
         context['has'] = r.non_empty_fields
         return {'score': score}, OK
 
@@ -279,20 +285,22 @@ class OnlineScoreHandler:
 class ClientsInterestsHandler:
 
     def process_request(self, request, context, store):
-        r = ClientsInterestsRequest(request.arguments)
-        if not r.is_valid():
-            return r.errors, INVALID_REQUEST
-        
-        context["nclients"] = len(r.client_ids)
-        response_body = {cid: scoring.get_interests(store, cid) for cid in r.client_ids}
+        clients_result = ClientsInterestsRequest(request.arguments)
+        if not clients_result.is_valid():
+            return clients_result.errors, INVALID_REQUEST
+        context["nclients"] = len(clients_result.client_ids)
+        response_body = {cid: scoring.get_interests(
+            store, cid) for cid in clients_result.client_ids}
         return response_body, OK
 
 
 def check_auth(request):
     if request.is_admin:
-        digest = hashlib.sha512(bytes(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT, "utf-8")).hexdigest()
+        digest = hashlib.sha512(bytes(datetime.datetime.now().strftime(
+            "%Y%m%d%H") + ADMIN_SALT, "utf-8")).hexdigest()
     else:
-        digest = hashlib.sha512(bytes(request.account + request.login + SALT, "utf-8")).hexdigest()
+        digest = hashlib.sha512(
+            bytes(request.account + request.login + SALT, "utf-8")).hexdigest()
     if digest == request.token:
         return True
     return False
@@ -309,7 +317,7 @@ def method_handler(request, ctx, store):
         return method_request.errors, INVALID_REQUEST
     if not check_auth(method_request):
         return "Forbidden", FORBIDDEN
-    
+
     handler = handlers[method_request.method]()
     return handler.process_request(method_request, ctx, store)
 
@@ -335,10 +343,12 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
 
         if request:
             path = self.path.strip("/")
-            logging.info("%s: %s %s" % (self.path, data_string, context["request_id"]))
+            logging.info("%s: %s %s" %
+                         (self.path, data_string, context["request_id"]))
             if path in self.router:
                 try:
-                    response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
+                    response, code = self.router[path](
+                        {"body": request, "headers": self.headers}, context, self.store)
                 except Exception as e:
                     logging.exception("Unexpected error: %s" % e)
                     code = INTERNAL_ERROR
@@ -351,7 +361,8 @@ class MainHTTPHandler(BaseHTTPRequestHandler):
         if code not in ERRORS:
             r = {"response": response, "code": code}
         else:
-            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+            r = {"error": response or ERRORS.get(
+                code, "Unknown Error"), "code": code}
         context.update(r)
         logging.info(context)
         self.wfile.write(bytes(json.dumps(r), "utf-8"))
@@ -366,7 +377,7 @@ if __name__ == "__main__":
     logging.basicConfig(filename=opts.log, level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
     server = HTTPServer(("localhost", opts.port), MainHTTPHandler)
-    logging.info("Starting server at %s" % opts.port)
+    logging.info(f"Starting server at {opts.port}")
     try:
         server.serve_forever()
     except KeyboardInterrupt:
