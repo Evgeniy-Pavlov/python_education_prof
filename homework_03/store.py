b48c8f5a9c9da69092bab84d46e8aa8224cd09ca
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2023-10-01 19:18:46 +0300

    добавлен store

--- homework_03/store.py
+++ homework_03/store.py
@@ -1,27 +1,27 @@
 import redis
 
 class Store:
-    def __init__(self, host='localhost', port=6379):
-        self.redis_connect = redis.Redis(host=host, port=port, retry_on_timeout=5)
 
-    def cache_set(self, key, value):
-        self.redis_connect.set(key, value)
+    def __init__(self, host='localhost', port=6379):
+        with redis.StrictRedis(host=host, port=port, retry_on_timeout=5, decode_responses=True) as red:
+            self.redis_connect = red
+
+    def cache_set(self, key, value, seconds=None):
+        if seconds:
+            result = self.redis_connect.set(key, value, seconds)
+        else:
+            result = self.redis_connect.set(key, value)
+        return result
     
     def get(self, key):
-        return (self.redis_connect.get(key)).decode()
+        return self.redis_connect.get(key)  
 
     def cache_get(self, key):
-        result_get =  (self.redis_connect.get(key)).decode()
+        result_get = (self.redis_connect.get(key))
         if result_get.isdecimal():
             return int(result_get, 10)
         else:
             return float(result_get)
         
 
-
-# redis_con = Store()
-
-# redis_con.cache_set(key='value1', value=122)
-
-# print(redis_con.get(key='value1'))
 
