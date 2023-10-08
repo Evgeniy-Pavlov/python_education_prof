bfad0a211e9d28f6f14fa82440594c9b8c8f77c9
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2023-10-08 23:10:23 +0300

    поправил парсинг строки запроса

--- homework_04/httpd.py
+++ homework_04/httpd.py
@@ -3,83 +3,85 @@ import socket
 import argparse
 import datetime
 import mimetypes
-from urllib import request
+from urllib import request, parse
 from multiprocessing import Process
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-r', type=str, help='Возвращать файлы по произвольному пути в DOCUMENT_ROOT', default='C:/Users/Evgeniy/ServerDir')
-    parser.add_argument('-w', type=int, help='Числов worker\'ов задается аргументом ĸомандной строĸи -w', default=1)
-    return parser.parse_args()
-
-parser = parse_arguments()
 
 OK = 200
 FORBIDDEN = 403
 NOT_FOUND = 404
 METHOD_NOT_ALLOWED = 405
+HEADERS = {'Date': datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S %Z'), 'Server': 'MyHTTPServer',
+           'Content-Length': 0, 'Content-Type': 'text/html', 'Connection': 'close'}
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', type=str, help='Возвращать файлы по произвольному пути в DOCUMENT_ROOT',
+                        default='C:/Users/Evgeniy/ServerDir')
+    parser.add_argument(
+        '-w', type=int, help='Числов worker\'ов задается аргументом ĸомандной строĸи -w', default=1)
+    return parser.parse_args()
+
+
+parser = parse_arguments()
 DOCUMENT_ROOT = parser.r
 WORKERS = parser.w
-RECV_STEP_SIZE = 10
-DELIMETER = '\r\n'
-
-HEADERS = {'Date': datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S %Z'), 'Server': 'MyHTTPServer', 'Content-Length': 0, 'Content-Type': 'text/html', 'Connection': 'close'}
-
 
 
 class RequestHandler:
-    
     def __init__(self, data):
         self.client_data = data.split('\r\n')
-        self.headers = []
-        self.body = str()
         self.user_headers = {}
 
     def check_query_params(self):
         list_user_request = self.user_headers['Request'].split('/')
         if '?' in list_user_request[-1]:
-            list_user_request[-1] = list_user_request[-1][:list_user_request[-1].index('?')]
+            list_user_request[-1] = list_user_request[-1][:
+                                                          list_user_request[-1].index('?')]
         return '/'.join(list_user_request)
 
     def parse_data(self):
         method, request, protocol = self.client_data[0].split(' ')
         self.user_headers['Method'] = method
-        self.user_headers['Request'] = request.replace('%20', ' ')
+        self.user_headers['Request'] = parse.unquote(request)
         self.user_headers['Protocol'] = protocol
         if self.user_headers['Request'].endswith('/'):
             self.user_headers['Request'] += 'index.html'
         self.user_headers['Request'] = self.check_query_params()
-        
 
     def create_headers(self, code, path=None):
         if code == OK:
             HEADERS['Content-Length'] = os.path.getsize(path)
-            HEADERS['Content-Type'] = mimetypes.guess_type(request.pathname2url(path))[0]
-            join_headers = '\r\n'.join([f'{x[0]}: {x[1]}' for x in HEADERS.items()])
+            HEADERS['Content-Type'] = mimetypes.guess_type(
+                request.pathname2url(path))[0]
+            join_headers = '\r\n'.join(
+                [f'{x[0]}: {x[1]}' for x in HEADERS.items()])
             HDRS = f'HTTP/1.1 {OK} OK\r\n{join_headers}\r\n\r\n'
             return HDRS
         elif code == FORBIDDEN:
             HEADERS['Content-Length'] = os.path.getsize(path)
-            HEADERS['Content-Type'] = mimetypes.guess_type(request.pathname2url(path))[0]
-            join_headers = '\r\n'.join([f'{x[0]}: {x[1]}' for x in HEADERS.items()])
+            HEADERS['Content-Type'] = mimetypes.guess_type(
+                request.pathname2url(path))[0]
+            join_headers = '\r\n'.join(
+                [f'{x[0]}: {x[1]}' for x in HEADERS.items()])
             HDRS = f'HTTP/1.1 {FORBIDDEN} FORBIDDEN\r\n{join_headers}\r\n\r\n'
             return HDRS
         elif code == NOT_FOUND:
-            join_headers = '\r\n'.join([f'{x[0]}: {x[1]}' for x in HEADERS.items() if x[0] not in ('Content-Type', 'Content-Length')])
+            join_headers = '\r\n'.join([f'{x[0]}: {x[1]}' for x in HEADERS.items(
+            ) if x[0] not in ('Content-Type', 'Content-Length')])
             HDRS = f'HTTP/1.1 {NOT_FOUND} NOT FOUND\r\n{join_headers}\r\n\r\n'
             return HDRS
         else:
-            join_headers = '\r\n'.join([f'{x[0]}: {x[1]}' for x in HEADERS.items() if x[0] not in ('Content-Type', 'Content-Length')])
+            join_headers = '\r\n'.join([f'{x[0]}: {x[1]}' for x in HEADERS.items(
+            ) if x[0] not in ('Content-Type', 'Content-Length')])
             HDRS = f'HTTP/1.1 {METHOD_NOT_ALLOWED} ERROR\r\n{join_headers}\r\n\r\n'
             return HDRS
-
 
     def find_attachment(self):
         response = ''
         with open(DOCUMENT_ROOT + self.user_headers['Request'], 'rb') as file:
             response = file.read()
         return response
-
 
     def send_response(self):
         try:
@@ -89,7 +91,8 @@ class RequestHandler:
             return HDRS.encode('utf-8')
         if self.user_headers['Method'] == 'GET':
             try:
-                HDRS = self.create_headers(code=OK, path=DOCUMENT_ROOT + self.user_headers['Request'])
+                HDRS = self.create_headers(
+                    code=OK, path=DOCUMENT_ROOT + self.user_headers['Request'])
             except FileNotFoundError:
                 HDRS = self.create_headers(code=NOT_FOUND)
                 return HDRS.encode('utf-8')
@@ -97,7 +100,8 @@ class RequestHandler:
             return HDRS.encode('utf-8') + content
         elif self.user_headers['Method'] == 'HEAD':
             try:
-                HDRS = self.create_headers(code=OK, path=DOCUMENT_ROOT + self.user_headers['Request'])
+                HDRS = self.create_headers(
+                    code=OK, path=DOCUMENT_ROOT + self.user_headers['Request'])
             except FileNotFoundError:
                 HDRS = self.create_headers(code=NOT_FOUND)
                 return HDRS.encode('utf-8')
@@ -112,14 +116,12 @@ class MyHTTPServer:
         self.hostname = host
         self.port = port
         self.mysocket = self.init_socket()
-        
 
     def init_socket(self):
         mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         mysocket.bind((self.hostname, self.port))
         mysocket.listen(10)
         return mysocket
-
 
     def server_runner(self):
         while True:
@@ -129,7 +131,6 @@ class MyHTTPServer:
             client_socket.send(request_handler.send_response())
             client_socket.shutdown(socket.SHUT_WR)
             client_socket.close()
-            
 
 
 if __name__ == '__main__':
