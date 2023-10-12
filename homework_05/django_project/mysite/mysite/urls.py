3aa19e46b617d0386b52127e52fe77cecf656ae9
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2023-10-12 16:48:44 +0300

    созданы представления

--- homework_05/django_project/mysite/mysite/urls.py
+++ homework_05/django_project/mysite/mysite/urls.py
@@ -20,5 +20,8 @@ from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', views.index),
+    path('', views.index, name='index'),
+    path('detail/<int:question_id>', views.detail),
+    path('results/<int:question_id>', views.results),
+    path('vote/<int:question_id>', views.vote),
 ]
--- homework_05/django_project/mysite/polls/admin.py
+++ homework_05/django_project/mysite/polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+@admin.register(Question)
+class AdminQuestion(admin.ModelAdmin):
+    list_display = ('id', 'question_text', 'pub_date')
+
+@admin.register(Choice)
+class AdminChoice(admin.ModelAdmin):
+    list_display = ('id', 'question', 'choice_text', 'votes')
