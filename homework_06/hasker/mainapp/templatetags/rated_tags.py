95e54d51634701d1535d00efa6790f0ba8945bb3
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2023-10-27 23:01:17 +0300

    сделал не полностью запрос для части treding

--- homework_06/hasker/mainapp/templatetags/rated_tags.py
+++ homework_06/hasker/mainapp/templatetags/rated_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Case, Value, When, Sum
 from mainapp.models import MTMQuestionRating, Question, UserBase, MTMReplyRating, Reply
 
 register = template.Library()
@@ -23,4 +24,16 @@ def reply_rated(context):
 def reply_counter_rated(context):
     result = len(MTMReplyRating.objects.filter(reply_rated=Reply.objects.get(id=context['reply'].id), is_positive=True))  - \
         len(MTMReplyRating.objects.filter(reply_rated=Reply.objects.get(id=context['reply'].id),is_positive=False))
+    return result
+
+@register.simple_tag(takes_context=True)
+def question_treding(context):
+    """select mm.question_rated_id , (sum(case when mm.is_positive then 1 else 0 end) - sum(case when is_positive then 0 else 1 end)) as rated
+        from mainapp_mtmquestionrating mm 
+        group by mm.question_rated_id 
+        order by rated desc
+        LIMIT 20"""
+    result = MTMQuestionRating.objects.annotate(rating = Sum(Case(When(is_positive=True, then=1)))\
+         - Sum(Case(When(is_positive=False, then=1)))).values('rating', 'question_rated').order_by('rating')
+    print(result)
     return result
