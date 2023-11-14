6ef59f5b5ab340a5887d0b9642b23d0d57e3f7a5
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2023-11-14 13:31:24 +0300

    подключил drf-yasg для работы сваггера

--- homework_06/hasker/hasker/settings.py
+++ homework_06/hasker/hasker/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'mainapp',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- homework_06/hasker/hasker/urls.py
+++ homework_06/hasker/hasker/urls.py
@@ -16,12 +16,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from mainapp.views import BasePageView, RegisterView, UserLoginView, UserLogoutView, UserProfileView, QuestionCreateView, QuetionDetailView, ReplyCreateView, \
     QuestionRatedUpView, QuestionRatedDownView, QuestionRatedCancelView, ReplyRatedUpView, ReplyRatedDownView, ReplyRatedCancelView, BestReplySetView,\
         SearchQuestionView, BasePageHotView
 from mainapp.api.views import BasePageAPIView, QuestionAPIView, SearchAPIView, ReplyAPIView, TrendingAPIView
-from django.conf.urls.static import static
-from django.conf import settings
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Hasker API",
+      default_version='v1',
+      description="Hasker description",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -47,6 +62,9 @@ urlpatterns = [
     path('api/v1/questions/search', SearchAPIView.as_view()),
     path('api/v1/question/<int:pk>/reply', ReplyAPIView.as_view()),
     path('api/v1/questions/trending', TrendingAPIView.as_view()),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('__debug__/', include('debug_toolbar.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
