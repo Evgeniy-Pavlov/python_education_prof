5e82f08a66d3c85014c78e6e13562cb0ec92130e
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2024-01-02 22:08:31 +0300

    скачивание страниц работает

--- homework_11/main.py
+++ homework_11/main.py
@@ -1,5 +1,7 @@
 import os
 import re
+import datetime
+import logging
 import argparse
 import asyncio
 import aiofiles
@@ -8,6 +10,8 @@ import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = 'https://news.ycombinator.com/'
+logging.basicConfig(format="[%(asctime)s] %(levelname).1s %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
+logger = logging.getLogger()
 news_dict = {}
 link_list = []
 
@@ -39,22 +43,22 @@ def find_href_in_comments(comments_pages):
         if result:
             link_list += result
 
-# async def find_href_in_comments(comments_page):
-#     global link_list
-#     async with aiohttp.ClientSession() as session:
-#         response = await session.request('GET', comments_page)
-#         page = await response.read()
-#         bs_page = BeautifulSoup(page, 'lxml')
-#         data = bs_page.find_all('div', class_='comment')
-#         result = [x.find('a').get('href') for x in data \
-#             if x.find('a') and 'https' in x.find('a').get('href')]
-#         if result:
-#             link_list += result
-#     return result
 
-
-def download_pages(news_dict):
-    pass
+async def download_page(link, parser):
+    filename = re.sub(r"[?:*<>,; /\\]", r'', link)
+    full_filename = os.path.join(parser.d, filename[:20])
+    if not news_dict[link]:
+        async with aiofiles.open(f'{full_filename}.html', mode='wb+') as handle:
+            async with aiohttp.ClientSession() as session:
+                try:
+                    response = await session.request('GET', link)
+                    page = await response.read()
+                    await handle.write(page)
+                except aiohttp.client_exceptions.InvalidURL as err:
+                    logger.info(f'Ссылка {link} сейчас недоступна.')
+                
+    else:
+        logger.info(f'Ссылка {link} уже была обработана ранее.')
 
 
 async def main():
@@ -66,8 +70,10 @@ async def main():
     for i in link_list:
         if i not in news_dict:
             news_dict[i] = False
-    print(news_dict)
-    return 0
+    logger.info(f'Сбор ссылок для скачивания страниц завершен. Всего найдено страниц {len(news_dict)}')
+    loop = asyncio.get_running_loop()
+    tasks = [loop.create_task(download_page(link, parser)) for link in news_dict.keys()]
+    await asyncio.gather(*tasks, return_exceptions=False)
 
 
 if __name__=="__main__":
