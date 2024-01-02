d4537342ecd7100d70c0d64e37fa9e69d08f3aba
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2024-01-02 20:33:54 +0300

    поменял механизм сбора ссылок из комментов на синхронный

--- homework_11/main.py
+++ homework_11/main.py
@@ -23,48 +23,52 @@ def parse_base_page():
     bs_page = BeautifulSoup(response.text, 'lxml')
     data = bs_page.find_all('span', class_='titleline')
     result = [x.find('a').get('href') for x in data]
-    return result
-
-
-def parse_comments_page():
-    response = requests.get(BASE_URL)
-    bs_page = BeautifulSoup(response.text, 'lxml')
     data = bs_page.find_all('a', string=re.compile('comments'))
-    result = [BASE_URL + x.get('href') for x in data[1:]]
-    return result
+    result_comment = [BASE_URL + x.get('href') for x in data[1:]]
+    return result, result_comment
 
 
-async def find_href_in_comments(comments_page):
+def find_href_in_comments(comments_pages):
     global link_list
-    async with aiohttp.ClientSession() as session:
-        response = await session.request('GET', comments_page)
-        page = await response.read()
-        bs_page = BeautifulSoup(page, 'lxml')
+    for page in comments_pages:
+        response = requests.get(page)
+        bs_page = BeautifulSoup(response.text, 'lxml')
         data = bs_page.find_all('div', class_='comment')
-        result = [x.find('a').get('href') for x in data if 'https' in x.find('a').get('href')]
+        result = [x.find('a').get('href') for x in data \
+            if x.find('a') and 'https' in x.find('a').get('href')]
         if result:
             link_list += result
-    return result
+
+# async def find_href_in_comments(comments_page):
+#     global link_list
+#     async with aiohttp.ClientSession() as session:
+#         response = await session.request('GET', comments_page)
+#         page = await response.read()
+#         bs_page = BeautifulSoup(page, 'lxml')
+#         data = bs_page.find_all('div', class_='comment')
+#         result = [x.find('a').get('href') for x in data \
+#             if x.find('a') and 'https' in x.find('a').get('href')]
+#         if result:
+#             link_list += result
+#     return result
 
 
-def download_many(comment_links):
-    loop = asyncio.get_event_loop()
-    to_do = [find_href_in_comments(page) for page in sorted(comment_links)]
-    wait_coro = asyncio.wait(to_do)
-    res, _ = loop.run_until_complete(wait_coro)
-    loop.close()
-    return len(res)
+def download_pages(news_dict):
+    pass
 
 
-def main():
+async def main():
     parser = parse_arguments()
     if not os.path.isdir(f'./{parser.d}'):
         os.mkdir(parser.d)
-    #link_list = parse_base_page()
-    comments_page_list = parse_comments_page()
-    download_many(comments_page_list)
+    link_list, comments_page_list = parse_base_page()
+    find_href_in_comments(comments_page_list)
+    for i in link_list:
+        if i not in news_dict:
+            news_dict[i] = False
+    print(news_dict)
     return 0
 
 
 if __name__=="__main__":
-    main()
+    asyncio.run(main())
