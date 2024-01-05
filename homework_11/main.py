96e2797eec4e336ea84577c6d35d1880edff2647
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2024-01-05 19:36:55 +0300

    добавил запуск функции обкачки в цикле с задержкой и документацию

--- homework_11/main.py
+++ homework_11/main.py
@@ -16,6 +16,9 @@ news_dict = {}
 link_list = []
 
 def parse_arguments():
+    """Функция парсинга аргументов. Принимается два аргумента:
+    -d - аргумент для указания папки для сохранения страниц,
+    -t - аргумент для указания количества секунд между запусками обкачки."""
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', type=str, help='Название папки для сохранения страниц', default='page_download')
     parser.add_argument('-t', type=int, help='Количество секунд между запусками обкачки новостного комбинатора', default=10)
@@ -23,6 +26,10 @@ def parse_arguments():
 
 
 def parse_base_page():
+    """Функция парсинга основной страницы новостного комбинатора. Функция возвращает кортеж со следующим содержимым:
+    result - результирующая переменная содержащая список ссылок на новости с главной страницы комбинатора,
+    result_comment - результирующая переменная содержащая список ссылок на комментарии к новостям, 
+    в дальнейшем используется для сборка ссылок уже внутри них."""
     response = requests.get(BASE_URL)
     bs_page = BeautifulSoup(response.text, 'lxml')
     data = bs_page.find_all('span', class_='titleline')
@@ -33,6 +40,11 @@ def parse_base_page():
 
 
 def find_href_in_comments(comments_pages):
+    """Функция поиска ссылок в комментариях. Принимает единственный аргумент comments_pages.
+    Ожидается, что это будет итерируемый объект (сейчас список), содержащий ссылки на комментарии.
+    Результатом выполенния функции является сбор ссылок со страниц комментариев к новостям. 
+    Новые ссылоки добавляются к уже существующей глобальной переменной link_list, сама функция
+    ничего не возвращает."""
     global link_list
     for page in comments_pages:
         response = requests.get(page)
@@ -45,6 +57,10 @@ def find_href_in_comments(comments_pages):
 
 
 async def download_page(link, parser):
+    """Асинхронная функция загрузки содержимого страниц. Принимает в качестве аргументов два значения:
+    link - ссылка на страницу (ожидается строка) и parser - объект содержащий аргументы при запуске скрипта.
+    Функция ничего не возвращает, а лишь выполняет запись страниц в указанной папке."""
+    global news_dict
     filename = re.sub(r"[?:*<>,; /\\]", r'', link)
     full_filename = os.path.join(parser.d, filename[:20])
     if not news_dict[link]:
@@ -54,6 +70,7 @@ async def download_page(link, parser):
                     response = await session.request('GET', link)
                     page = await response.read()
                     await handle.write(page)
+                    news_dict[link] = True
                 except aiohttp.client_exceptions.InvalidURL as err:
                     logger.info(f'Ссылка {link} сейчас недоступна.')
                 
@@ -61,19 +78,31 @@ async def download_page(link, parser):
         logger.info(f'Ссылка {link} уже была обработана ранее.')
 
 
-async def main():
-    parser = parse_arguments()
-    if not os.path.isdir(f'./{parser.d}'):
-        os.mkdir(parser.d)
+async def loop_runner(parser):
+    """Асинхронная функция запуска списка заданий.
+    Функция ничего не возвращает, лишь выполняет запуск функций парсинга страниц и их загрузки."""
+    global news_dict
     link_list, comments_page_list = parse_base_page()
     find_href_in_comments(comments_page_list)
     for i in link_list:
         if i not in news_dict:
             news_dict[i] = False
-    logger.info(f'Сбор ссылок для скачивания страниц завершен. Всего найдено страниц {len(news_dict)}')
     loop = asyncio.get_running_loop()
     tasks = [loop.create_task(download_page(link, parser)) for link in news_dict.keys()]
     await asyncio.gather(*tasks, return_exceptions=False)
+
+
+async def main():
+    parser = parse_arguments()
+    if not os.path.isdir(f'./{parser.d}'):
+        os.mkdir(parser.d)
+    while True:
+        try:
+            await loop_runner(parser)
+        except OSError as err:
+            logger.info(f"Возникла ошибка при попытке загрузки страницы. {err}")
+        await asyncio.sleep(parser.t)
+    logger.info(f'Сбор ссылок для скачивания страниц завершен. Всего найдено страниц {len(news_dict)}')
 
 
 if __name__=="__main__":
