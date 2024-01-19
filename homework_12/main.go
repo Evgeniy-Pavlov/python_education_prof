182026b1d2b8189896a3e5b56ca24d83205bd438
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2024-01-19 22:33:00 +0300

    сделал переименование файлов после обработки

--- homework_12/main.go
+++ homework_12/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+)
 
 func main() {
 
-	fmt.Println("Hello world!")
+	pattern_files := "data/appsinstalled/*.tsv.gz"
+	files, err := filepath.Glob(pattern_files)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		path_file := filepath.ToSlash(path.Clean(file))
+		dir, file_name := path.Split(path_file)
+		new_path := path.Join(dir, "."+file_name)
+		err := os.Rename(path_file, new_path)
+		if err != nil {
+			fmt.Println("Не удалось переименовать файл из-за ошибки: ", err)
+		} else {
+			fmt.Println("Файл переименован: ", new_path)
+		}
+	}
 
 }
