9566f98773cab55e7e5ca13284ac3d7db11043b7
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2024-01-24 14:59:41 +0300

    реализовал чтение файла

--- homework_12/main.go
+++ homework_12/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path"
 	"path/filepath"
+	"sync"
 
 	"github.com/bradfitz/gomemcache/memcache"
 )
@@ -21,6 +25,43 @@ type str_parse struct {
 	apps        string
 }
 
+func get_rawlines_from_file(path_file string) (chan []byte, chan error, error) {
+	rawlines := make(chan []byte)
+	errs := make(chan error)
+	file, err := os.Open(path_file)
+	if err != nil {
+		log.Println("Невозможно открыть указанный файл, возникла ошибка:", err)
+	}
+	defer file.Close()
+	file_reader, err := gzip.NewReader(file)
+	if err != nil {
+		log.Println("Возникла ошибка при попытке открыть файл:", err)
+	}
+	buff_contents := bufio.NewReader(file_reader)
+
+	go func(rawlines chan []byte, errs chan error, contents *bufio.Reader) {
+		defer func(rawlines chan []byte, errs chan error) {
+			close(rawlines)
+			close(errs)
+		}(rawlines, errs)
+		for {
+			line, err := buff_contents.ReadBytes('\n')
+			rawlines <- line
+			if err != nil {
+				if err != io.EOF {
+					errs <- err
+				}
+				return
+			}
+		}
+	}(rawlines, errs, buff_contents)
+	return rawlines, errs, nil
+}
+
+func parse_lines_file(rawlines chan []byte) int {
+	return 0
+}
+
 func main() {
 
 	device_memc := make(map[string]*string, 4)
@@ -29,6 +70,8 @@ func main() {
 	device_memc["gaid"] = flag.String("gaid", "127.0.0.1:33014", "Адрес и порт куда будут отправляться данные для gaid")
 	device_memc["adid"] = flag.String("adid", "127.0.0.1:33015", "Адрес и порт куда будут отправляться данные для adid")
 	device_memc["dvid"] = flag.String("dvid", "127.0.0.1:33016", "Адрес и порт куда будут отправляться данные для dvid")
+	workers := flag.Int("workers", 10, "Число воркеров для обработки файла")
+	wg := new(sync.WaitGroup)
 	flag.Parse()
 	fmt.Println(device_memc)
 
@@ -46,14 +89,28 @@ func main() {
 
 	for _, file := range files {
 		path_file := filepath.ToSlash(path.Clean(file))
+		rawlines, _, err := get_rawlines_from_file(path_file)
+		if err != nil {
+			log.Println("Возникла ошибка при чтении файла: ", err)
+		}
+		for i := 0; i < *workers; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				parse_line := parse_lines_file(rawlines)
+				fmt.Println(parse_line)
+			}()
+		}
 		dir, file_name := path.Split(path_file)
 		new_path := path.Join(dir, "."+file_name)
-		err := os.Rename(path_file, new_path)
+		err = os.Rename(path_file, new_path)
 		if err != nil {
 			fmt.Println("Не удалось переименовать файл из-за ошибки: ", err)
 		} else {
 			fmt.Println("Файл переименован: ", new_path)
 		}
+
 	}
+	wg.Wait()
 
 }
