be9d0d687cc915a1ea2519cfae0caba6dd675499
Evgeniy-Pavlov <ewgenypavlov@yandex.ru>
2024-01-25 23:06:44 +0300

    скрипт работает, но на некоторых строках падает

--- homework_12/main.go
+++ homework_12/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"compress/gzip"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -10,7 +11,10 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"sync"
+	"sync/atomic"
 
 	"github.com/bradfitz/gomemcache/memcache"
 )
@@ -20,9 +24,14 @@ const NORMAL_ERR_RATE = 0.01
 type str_parse struct {
 	device_type string
 	dev_id      string
-	lat         float32
-	lon         float32
+	lat         float64
+	lon         float64
 	apps        string
+}
+
+type apps struct {
+	device_type string
+	value       []byte
 }
 
 func get_rawlines_from_file(path_file string) (chan []byte, chan error, error) {
@@ -58,8 +67,70 @@ func get_rawlines_from_file(path_file string) (chan []byte, chan error, error) {
 	return rawlines, errs, nil
 }
 
-func parse_lines_file(rawlines chan []byte) int {
-	return 0
+func parse_lines_file(done chan struct{}, rawlines chan []byte) chan apps {
+	var st_p str_parse
+	var msg apps
+	parsed_lines := make(chan apps)
+
+	go func() {
+		defer close(parsed_lines)
+		for rawline := range rawlines {
+			line := string(rawline)
+			line_split := strings.Split(line, "\t")
+			if len(line_split) < 4 {
+				log.Println("Строка пустая или не может быть использована для парсинга")
+			}
+
+			if line_split[0] != "idfa" && line_split[0] != "gaid" && line_split[0] != "adid" && line_split[0] != "dvid" {
+				log.Println(line_split[0], " не является используемым для обработки типом устройства")
+			} else {
+				st_p.device_type = line_split[0]
+			}
+			st_p.dev_id = line_split[1]
+			fmt.Println(line_split)
+			lat, err := strconv.ParseFloat(line_split[2], 32)
+			if err != nil {
+				log.Println("Указанное значение невозможно преобразовать в lat float64")
+			} else {
+				st_p.lat = lat
+			}
+
+			lon, err := strconv.ParseFloat(line_split[3], 32)
+			if err != nil {
+				log.Println("Указанное значение невозможно преобразовать в lon float64")
+			} else {
+				st_p.lon = lon
+			}
+			st_p.apps = line_split[4]
+			msg.device_type = st_p.device_type
+			value, err := json.Marshal(st_p)
+			if err != nil {
+				log.Println("Невозможно сериализовать указанную структуру")
+			}
+			msg.value = value
+			msg.device_type = st_p.device_type
+
+			select {
+			case parsed_lines <- msg:
+			case <-done:
+				return
+			}
+
+		}
+	}()
+	return parsed_lines
+}
+
+func send_to_memc(parsed_lines chan apps, connector map[string]*memcache.Client, success *uint64, errors *uint64) {
+	for line := range parsed_lines {
+		atomic.AddUint64(success, 1)
+		client := connector[line.device_type]
+		err := client.Set(&memcache.Item{Key: line.device_type, Value: line.value})
+		if err != nil {
+			atomic.AddUint64(errors, 1)
+			continue
+		}
+	}
 }
 
 func main() {
@@ -73,7 +144,8 @@ func main() {
 	workers := flag.Int("workers", 10, "Число воркеров для обработки файла")
 	wg := new(sync.WaitGroup)
 	flag.Parse()
-	fmt.Println(device_memc)
+	done_ch := make(chan struct{})
+	var success, errors uint64
 
 	memcache_connector := make(map[string]*memcache.Client, 4)
 
@@ -97,8 +169,8 @@ func main() {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				parse_line := parse_lines_file(rawlines)
-				fmt.Println(parse_line)
+				parse_line := parse_lines_file(done_ch, rawlines)
+				send_to_memc(parse_line, memcache_connector, &success, &errors)
 			}()
 		}
 		dir, file_name := path.Split(path_file)
@@ -112,5 +184,11 @@ func main() {
 
 	}
 	wg.Wait()
+	rate := float64(errors) / float64(success)
+	if rate < NORMAL_ERR_RATE {
+		fmt.Println("Загрузка данных прошла успешно, процент ошибок: ", rate)
+	} else {
+		fmt.Println("Загрузка данных прошла с ошибками, процент ошибок: ", rate)
+	}
 
 }
